775f5d6d80a21894360f01bdbd673b0f1754965e
Rodrigo Sinope 70239 <lourenco.rs@gmail.com>
2019-09-13 11:29:14 -0300

    Estilizando pagina Main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
-import { Title } from './styles';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+
+import { Container, Title, Form, SubmitButton } from './styles';
 
 export default function Main() {
-  return <Title>Main</Title>;
+  return (
+    <Container>
+      <Title>
+        <FaGithubAlt />
+        Repositórios
+      </Title>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar repositório" />
+
+        <SubmitButton disabled>
+          <FaPlus color="#FFF" size="14px" />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
